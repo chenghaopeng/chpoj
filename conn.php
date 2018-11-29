fc58beb6cbb5f9a88e17603d30af3c18281fbed7
root <root@host.localdomain>
2018-11-29 11:04:43 -0500

    删除连接文件

--- conn.php
+++
@@ -1,5 +0,0 @@
-<?php   
-    $conn = mysqli_connect("localhost","chp","85203203") or die("Database access error:".mysql_error());  
-    mysqli_select_db($conn,"oj_chper_cn") or die("Database visit error:".mysql_error());  
-    mysqli_query($conn , "set names utf8");
-?>
